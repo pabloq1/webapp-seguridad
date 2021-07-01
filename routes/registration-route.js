4acbec0032252a345bdc1578557902346b959419
Pablo Quagliata <pablo.quagliata@gmail.com>
2021-06-30 22:11:39 -0300

    Format

--- routes/registration-route.js
+++ routes/registration-route.js
@@ -20,22 +20,21 @@ router.post('/register', function(req, res, next) {
     }
 // check unique email address
 var sql='SELECT * FROM registration WHERE email_address =?';
-db.query(sql, [inputData.email_address] ,function (err, data, fields) {
- if(err) throw err
- if(data.length>1){
-     var msg = inputData.email_address+ "was already exist";
- }else if(inputData.confirm_password != inputData.password){
-    var msg ="Password & Confirm Password is not Matched";
-me  }else{
-     
+db.query(sql, [inputData.email_address], function (err, data, fields) {
+    if(err) throw err
+    if(data.length>1){
+        var msg = inputData.email_address+ "was already exist";
+    } else if (inputData.confirm_password != inputData.password){
+            var msg ="Password & Confirm Password is not Matched";
+    } else {
     // save users data into database
-    var sql = 'INSERT INTO registration SET ?';
-   db.query(sql, inputData, function (err, data) {
-      if (err) throw err;
+        var sql = 'INSERT INTO registration SET ?';
+        db.query(sql, inputData, function (err, data) {
+        if (err) throw err;
            });
-  var msg ="Your are successfully registered";
- }
- res.render('registration-form',{alertMsg:msg});
+    var msg ="Your are successfully registered";
+    }
+    res.render('registration-form',{alertMsg:msg});
 })
      
 });
