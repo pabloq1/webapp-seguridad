4c453d1c695b45295126f17fecbd3241a744ebe8
Pablo Quagliata <pablo.quagliata@gmail.com>
2021-07-01 20:06:29 -0300

    WIP Registration Logic

--- routes/registration-route.js
+++ routes/registration-route.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var db=require('../database');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = `${process.env.PLAIN_PASS}`;
 
 // to display registration form 
 router.get('/register', function(req, res, next) {
   res.render('registration-form');
 });
 
-// to store user input detail on post request
+/**
+ * INPUT LOGIC on POST
+ */
+
 router.post('/register', function(req, res, next) {
     
     inputData ={
@@ -18,24 +24,35 @@ router.post('/register', function(req, res, next) {
         password: req.body.password,
         confirm_password: req.body.confirm_password
     }
-// check unique email address
-var sql='SELECT * FROM registration WHERE email_address =?';
-db.query(sql, [inputData.email_address], function (err, data, fields) {
-    if(err) throw err
-    if(data.length > 1){
-        var msg = inputData.email_address+ "was already exist";
-    } else if (inputData.confirm_password != inputData.password){
-            var msg ="Password & Confirm Password is not Matched";
-    } else {
-    // save users data into database
-        var sql = 'INSERT INTO registration SET ?';
-        db.query(sql, inputData, function (err, data) {
-        if (err) throw err;
-           });
-    var msg ="Your are successfully registered";
-    }
-    res.render('registration-form',{alertMsg:msg});
-})
+
+    /**
+     * DATABASE LOGIC
+     */
+
+    // check unique email address
+    var sql='SELECT * FROM registration WHERE email_address =?';
+    db.query(sql, [inputData.email_address], function (err, data, fields) {
+        if(err) throw err
+
+        if(data.length > 1){
+            var msg = inputData.email_address+ "was already exist";
+        } else if (inputData.confirm_password != inputData.password){
+                var msg ="Password & Confirm Password is not Matched";
+        } else {
+        
+        /* -- SAVE USER INTO DATABASE -- */
+            /* -- HASH -- */
+            inputData.password = bcrypt.hashSync(inputData.password, saltRounds);
+            inputData.confirm_password = bcrypt.hashSync(inputData.confirm_password, saltRounds);
+            var sql = 'INSERT INTO registration SET ?';
+            db.query(sql, inputData, function (err, data) {
+            if (err) throw err;
+            });
+        var msg ="Your are successfully registered";
+        }
+
+        res.render('registration-form',{alertMsg:msg});
+    })
      
 });
 module.exports = router;
