4b014e8a9075d8b0c181b6743f2de1886e309013
Pablo Quagliata <pablo.quagliata@gmail.com>
2021-07-01 20:49:08 -0300

    Format

--- routes/registration-route.js
+++ routes/registration-route.js
@@ -16,16 +16,6 @@ router.get('/register', function(req, res, next) {
  * INPUT LOGIC on POST
  */
 
-function passwordValidation(password) {
-    var numeric_characters = /[0-9]/;
-    var special_characters = /[#?!@$%^&*\-_\\\/]/;
-    var capital_letters = /[A-Z]/;
-    return password.match(special_characters) != undefined && 
-            password.match(capital_letters) != undefined && 
-            password.match(numeric_characters) != undefined && 
-            password.length >= 8;
-};
-
 router.post('/register', function(req, res, next) {
     inputData ={
         first_name: req.body.first_name,
