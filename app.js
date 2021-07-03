22c97076e4e8b2258653c17056ea32da6d4f00c8
Pablo Quagliata <pablo.quagliata@gmail.com>
2021-07-03 20:53:46 -0300

    Removed SQL lines

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const dotenv = require('dotenv').config();
 const session = require('express-session');
 const app = express();
 
+// -----------------
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
