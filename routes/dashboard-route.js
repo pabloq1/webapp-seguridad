f10d24731f47ebd849c0af73e05454f5746702ea
Ale Etchevers <82960016+AleEtcheversEager@users.noreply.github.com>
2021-07-05 19:26:44 -0300

    Empezamos dashboard

--- routes/dashboard-route.js
+++ routes/dashboard-route.js
@@ -7,12 +7,60 @@ const constants = require('../utils/constants')
 /* GET dashboard form */
 router.get('/dashboard', function(req, res, next) {
     if (req.session.loggedInUser) {
-        res.render('dashboard-form', {email:req.session.emailAddress})
+        res.render('dashboard-form', { email: req.session.emailAddress })
     } else {
         res.redirect('/user/register')
     }
-  });
+});
+
+router.post('/dashboard', function(req, res, next) {
+    if (req.session.loggedInUser) {
+        obtenerGruposDeUsuario(req.session.loggedInUser)
+        res.render('dashboard-form', { email: req.session.emailAddress }, groups)
+
+    }
+});
+
+function obtenerGruposDeUsuario(emailUser) {
+    SQL_STATEMENT = `SELECT nombreGrupo FROM ${process.env.DB_USUARIO_GRUPO_TABLE} WHERE nombreUser =?`;
+    // return result.get('nombreGrupo'); //Lista de Strings ["GrupoX","GRupoY","GrupoZ"]
+    db.query(SQL_STATEMENT, [req.session.emailAddress], function(err, query_result, fields) {
+
+    })
+
+}
+
+function obtenerRecursosGrupoUser(nombreGrupo, nombreUser) {
+
+}
+
+//   obtenerRecursosGrupoUser(nombreGrupo,userName){
+//     resultSet = SELECT * FROM UsuarioGrupo WHERE nombreUser = userName AND nombreGrupo = nombreGrupo;
+//     if(!resultSet.empty){ // Pertenece al Grupo
+//       resultSetRecursos = SELECT nombreRecurso FROM Recurso WHERE grupo = nombreGrupo;
+//       if(!resultSetRecursos.empty){
+//         return resultSetRecursos.get("nombreRecurso") // Listado de 'nombreRecurso' que son Strings
+//       }else{
+//         return 'El Grupo ' + nombreGrupo + ' no tiene recursos'
+//       }
+//     }else{
+//       return 'Usted no pertenece al Grupo'
+//     }
+//   }
+
+//   subirArchivo(emailUser,nombreGrupo, archivo,nombreRecurso){
+//     // Ver como es mandar un MEDIUMBLOB a la DB y como tomarlo desde la Web
+//     resultSet = SELECT * FROM UsuarioGrupo WHERE nombreUser = emailUser, nombreGrupo = nombreGrupo , agregar = True;
+//     if(!resultSet.empty){
+//       Date fecha = Date.now() // ver en javascript
+//       query = INSERT INTO Recurso(nombreRecurso, fecha.ToString , nombreGrupo , archivo)
+//       resultadoInsert = conn.ejecutarQuery(query)
+//       if(resultadoInsert exitoso){
+//         return 'Archivo agregado satisfactoriamente'
+//       }else{
+//         return 'Ha ocurrido un problema'
+//       }
+//     }
+//   }
 
 module.exports = router;
-
-
--- routes/registration-route.js
+++ routes/registration-route.js
@@ -36,38 +36,38 @@ router.post('/register', function(req, res, next) {
         if(query_result.length > 0){
             // check unique email address
             var msg = constants.OTHER_EMAIL
-            res.render('registration-form', { alertMsg:msg,     
+            res.render('registration-form', { alertMsg:msg,
                 passwordInfo:constants.PASSWORD_CHARACTERS });
         } else if (req.body.confirm_password != inputUserData.password){
             var msg = constants.PASSWORDS_NOT_MATCHING
-            res.render('registration-form', { alertMsg:msg,     
+            res.render('registration-form', { alertMsg:msg,
                 passwordInfo:constants.PASSWORD_CHARACTERS });
         } else if (!validation(inputUserData.password)) {
             var msg = constants.PASSWORD_FORMAT
-            res.render('registration-form', { alertMsg:msg,     
+            res.render('registration-form', { alertMsg:msg,
                 passwordInfo:constants.PASSWORD_CHARACTERS });
         } else {
             db.query(sql_group, [inputGroupData.nombreGrupo], function(err, query_group_result, fields){
                 if (query_group_result.length > 0){
                     var msg = constants.GROUP_NAME_EXISTS
-                    res.render('registration-form', { alertMsg:msg,     
+                    res.render('registration-form', { alertMsg:msg,
                         passwordInfo:constants.PASSWORD_CHARACTERS });
                 } else {
                     inputUserData.password = bcrypt.hashSync(inputUserData.password, saltRounds)
-        
+
                     var sql_email = `INSERT INTO ${process.env.DB_USUARIO_TABLE} SET ?`
                     var sql_group = `INSERT INTO ${process.env.DB_GRUPO_TABLE} SET ?`
-        
+
                     db.query(sql_email, [inputUserData], function (err, query_result) {
                         if (err) throw err;
                     })
-        
+
                     db.query(sql_group, [inputGroupData], function (err, query_result) {
                         if (err) throw err;
                     })
 
                     var msg = constants.REGISTER_SUCCESS;
-                    res.render('registration-form', { alertMsg:msg,     
+                    res.render('registration-form', { alertMsg:msg,
                         passwordInfo:constants.PASSWORD_CHARACTERS });
                 }
             })
